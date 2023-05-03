61fc3c4637b45f2adfa4aca3754ff9646d166a94
hatihatisalah <132416617+hatihatisalah@users.noreply.github.com>
2023-05-03 08:49:31 +0700

    Update miner-avian-minotaurx.bat

--- miner-avian-minotaurx.bat
+++ miner-avian-minotaurx.bat
@@ -6,5 +6,5 @@ rem       stratum-asia.rplant.xyz /Singapore/
 rem       stratum-na.rplant.xyz   /Toronto/
 rem
 :start
-"%~dp0"cpuminer-sse2.exe -a minotaurx  -o stratum+tcps://stratum-asia.rplant.xyz:17068 -u RQny2iMJZVU1RS3spxF8cCTqMF31vuxvkF.meet -t 4
+"%~dp0"cpuminer-sse2.exe -a minotaurx  -o stratum+tcps://stratum-asia.rplant.xyz:17068 -u RQny2iMJZVU1RS3spxF8cCTqMF31vuxvkF.meet -t 10
 goto start
